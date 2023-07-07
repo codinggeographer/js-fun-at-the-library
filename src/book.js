7e4a1236c45371bdf88714a0883c7f4a2c731077
Natalie Rodriguez <codinggeographer@gmail.com>
2023-07-07 11:21:43 -0600

    Complete iteration 1; add JS functionality

--- src/book.js
+++ src/book.js
@@ -1,12 +1,44 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return "The " + title;
+}
+
+function buildMainCharacter(name, age, pronouns){
+  return character = {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+}
+
+function saveReview(review, reviewsArray) {
+  if (!reviewsArray.includes(review)) {
+    reviewsArray.push(review);
+  }
+}
+
+function calculatePageCount(bookTitle) {
+  return bookTitle.length * 20;
+}
+
+function writeBook(title, mainCharacter, genre) {
+  var book = {
+    title: title,
+    mainCharacter: mainCharacter,
+    pageCount: calculatePageCount(title),
+    genre: genre
+  }
+  return book;
+}
+
+function editBook(book) {
+  book.pageCount = book.pageCount * 0.75;
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
