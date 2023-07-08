fb5c76b37263ac70e64768439f4b1bdc32da5fda
Natalie Rodriguez <codinggeographer@gmail.com>
2023-07-08 13:12:56 -0600

    Complete shelf tests in iteration 2

--- src/library.js
+++ src/library.js
@@ -1,8 +1,22 @@
+function createLibrary(name) {
+  var library = {
+    name: name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: []
+    }
+  }
+  return library;
+}
 
+function addBook(library, book) {
+  library.shelves[book.genre].push(book);
+}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
+  createLibrary,
+  addBook,
   // checkoutBook,
   // takeStock
 };
--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,36 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    shelf.unshift(book)
+  }
+};
+
+function unshelfBook(bookTitle, shelf) {
+  for (var i = 0; i < shelf.length; i++)
+    if (shelf[i].title === bookTitle) {
+      shelf.splice(i, 1)
+    }
+};
+
+function listTitles(shelf) {
+  var titles = []
+  for (var i = 0; i < shelf.length; i++) {
+    titles.push(shelf[i].title)
+  }
+  return titles.join(", ")
+};
+
+function searchShelf(shelf, bookTitle) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === bookTitle) {
+      return true
+    }
+  }
+      return false
+};
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
